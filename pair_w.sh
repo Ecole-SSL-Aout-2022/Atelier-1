9ae47a7f8e8f88f2c0d2e95d43af0d74f04c3a7e
Wanchai290 <wanchai.menier@gmail.com>
2022-09-06 20:03:21 +0200

    Added other cases in pairing & completed main

--- pair_w.sh
+++ pair_w.sh
@@ -6,15 +6,20 @@ function discover_rsk_devices() {
 	# Start discovering bluetooth devices for n seconds
 	timeout 5 bluetoothctl scan on
 	# Grab MAC adresses of only RSK devices
-	devices=$(bluetoothctl devices | grep RSK | cut -f2 -d" ")
+	devices=$(bluetoothctl devices | grep RSK)
 	return $devices
 }
 
 # Pairs a single BT device
+# Params :
+# 	device - MAC Adress of a RSK robot
 function pair_robot() {
+	
+	device_name=$1
+	device_mac=$2
 
 	# Starts an asynchronous pairing
-	coproc BTCTL (bluetoothctl pair $device)
+	coproc BTCTL (bluetoothctl pair $device_mac)
 
 	# Grab pairing status, to guess if we need to enter PIN or just type in yes
 	# We use the file descriptor of the asynchronous process
@@ -25,12 +30,20 @@ function pair_robot() {
 			# Send 1234 to ${BTCTL[1]} which is like a user-input
 			echo "1234" >& "${BTCTL[1]}"
 			sleep 1 # wait a bit to finish pairing
-			break #todo : check if paired correctly ? if not increase sleep time ?
 
 		elif [ echo $line | grep -q "Confirm passkey" ]; then
 			echo "y" >& "${BTCTL[1]}"
+
 		elif [ line = "Pairing successful" ];
+			paired=$(bluetoothctl paired-devices | grep $device_mac)
+			if [ $paired -eq "" ]; then
+				echo "Pairing of ${device_name} has failed, please pair manually"
+				echo "$device_name\n" > $LOG_FILE
+			else;
+				echo "${device_name} paired successfully !"
+			fi
 			break
+
 		fi
 	done
 
@@ -40,7 +53,17 @@ function pair_robot() {
 }
 
 function main() {
-	devices=$(discover_rsk_devices)
+	LOG_FILE="failed_pairings.log"
+	new_devices=$(discover_rsk_devices)
+	#alr_paired=$(bluetoothctl paired-devices)
+	dev_macs=$(echo $"{new_devices}" | cut -f2 -d" ")
+
+	# TODO : check for already paired devices
+	for devi in $new_devices do
+		devi_name=$(echo $devi | cut -f1 -d" ")
+		devi_mac=$(echo $devi | cut -f2 -d" ")
+		pair_robot $devi_name $devi_mac
+	done
 }
 
 main
