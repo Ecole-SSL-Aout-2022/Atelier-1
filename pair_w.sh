28853f26833ac8805d59ad8c95ae15f8094a653e
MENIER Thomas Wanchaï <wanchai.menier@gmail.com>
2022-09-15 20:11:33 +0200

    Passes shell check and fixed more bugs

--- pair_w.sh
+++ pair_w.sh
@@ -5,7 +5,7 @@
 function grab_rsk_devices() {
 	# Grab MAC adresses of only RSK devices
 	devices=$(bluetoothctl devices | grep SHIELD)
-	echo $devices
+	echo "$devices"
 }
 
 # Pairs a single BT device
@@ -16,10 +16,10 @@ function pair_robot() {
 	device_name=$1
 	device_mac=$2
 
-	echo "${device_name} - ${device_mac}\n" >> $BT_LOG_FILE
+	printf "%s- %s\n" "${device_name}" "${device_mac}" >> $BT_LOG_FILE
 
 	# Starts an asynchronous pairing
-	coproc BTCTL (bluetoothctl pair $device_mac)
+	coproc BTCTL (bluetoothctl pair "$device_mac")
 
 	# Grab pairing status, to guess if we need to enter PIN or just type in yes
 	# We use the file descriptor of the asynchronous process
@@ -27,23 +27,24 @@ function pair_robot() {
 	while IFS= read -r -u line;
 	do
 		# Log the output
-		echo $line >> $BT_LOG_FILE
-
-		# Check whether we confirm passkey or enter pin code
-		if [ echo $line | grep -q "Enter PIN code:" ]; then
+		echo "$line" >> $BT_LOG_FILE
+		# Check whether we confirm passkey or enter pin code
+		if [[ $(echo "$line" | grep -q "Enter PIN code:") ]]
+		then
 			# Send 1234 to ${BTCTL[1]} which is like a user-input
 			echo "1234" >& "${BTCTL[1]}"
 			sleep 1 # wait a bit to finish pairing
 
-		elif [ echo $line | grep -q "Confirm passkey" ]; then
+		elif [ "$(echo "$line" | grep -q "Confirm passkey")" ]
+		then
 			# Just reply yes to pair device
 			echo "y" >& "${BTCTL[1]}"
 
 		# In case pairing is successful
-		elif [ $line -eq "Pairing successful" ]; then
+		elif [ "$($line -eq "Pairing successful")" ]; then
 			# Another check to see if device paired correctly (is this necessary ? probably not)
-			paired=$(bluetoothctl paired-devices | grep $device_mac)
-			if [ $paired -eq "" ]; then
+			paired=$(bluetoothctl paired-devices | grep "$device_mac")
+			if [ "$("$paired" -eq "")" ]; then
 				echo "Unknown Error : Pairing of ${device_name} has failed, please pair manually"
 				echo "$device_name\n" > $FAILED_LOG_FILE
 			else
@@ -51,53 +52,61 @@ function pair_robot() {
 			fi
 			break
 
-		elif [ echo $line | grep -q "Failed to pair" ]; then
-			echo "${device_name} could not be paired. Reason : ${line} \n Check ${BT_LOG_FILE} for more info"
+		elif [ "$(echo "$line" | grep -q "Failed to pair")" ]; then
+			printf "%s could not be paired. Reason : %s \n Check %s for more info" "${device_name}" "${line}" "${BT_LOG_FILE}"
 			break
 		fi
 	done < "${BTCTL[0]}"
 
 	# After pairing, we kill the coprocess
-	kill -9 $BTCTL_PID
+	kill -9 "$BTCTL_PID"
 
 }
 
 function log_setup() {
 	FAILED_LOG_FILE="failed_pairings.log"
-	echo "" > $FAILED_LOG_FILE
+	echo " " > $FAILED_LOG_FILE
 	BT_LOG_FILE="bluetoothctl_pairing.log"
-	echo "" > $BT_LOG_FILE
+	echo " " > $BT_LOG_FILE
 	SCAN_LOG_FILE="bluetoothctl_scan_on.log"
-	echo "" > $SCAN_LOG_FILE
+	echo " " > $SCAN_LOG_FILE
+	NEW_DEVI_FILE="new_devices.log"
+	echo " " > $NEW_DEVI_FILE
 }
 
 function main() {
 	log_setup
 	echo "Starting discovery..."
 	# Start discovering bluetooth devices in the background
-	bluetoothctl scan on & >>$SCAN_LOG_FILE 2>>$SCAN_LOG_FILE
+	bluetoothctl scan on & >> $SCAN_LOG_FILE 2>> $SCAN_LOG_FILE
 	scanpid=$!
 	# Wait for devices to be discovered
 	sleep 3
-
+	echo "Discovery ended"
 	new_devices=$(grab_rsk_devices)
+	echo "$new_devices" > $NEW_DEVI_FILE
 	alr_paired=$(bluetoothctl paired-devices)
 
 	# Stop the discovery process
 	kill -9 $scanpid
 
-	for devi in $new_devices;
+	while IFS="\n" read -r devi
 	do
-		devi_name=$(echo $devi | cut -f1 -d" ")
-		devi_mac=$(echo $devi | cut -f2 -d" ")
+		echo "$devi"
+		devi_name=$(echo "$devi" | cut -f3 -d" ")
+		devi_mac=$(echo "$devi" | cut -f2 -d" ")
+
+		echo "$devi_name"
+		echo "$devi_mac"
 
 		# Dupe check taken from RSK's github - pair.sh script
-		dupe=$(echo $alr_paired | grep $devi_mac)
-		echo "Current dupe is : ${dupe}"
+		dupe=$(echo "$alr_paired" | grep "$devi_mac")
+		echo "Dupe is $dupe"
 		if [ -z "$dupe" ]; then
-			pair_robot $devi_name $devi_mac
+			pair_robot "$devi_name" "$devi_mac"
 		fi
-	done
+		break
+	done < $NEW_DEVI_FILE
 }
 
 main
